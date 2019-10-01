cfddf28acd7e66afafb7aaf61c73f1a93097c4de
Ran-Esther-Dou <randou@brandeis.edu>
2019-10-01 09:55:43 -0400

    2019.09.30 update

--- LeetCode_Algorithms_Easy.py
+++ LeetCode_Algorithms_Easy.py
@@ -6,7 +6,35 @@ Created on Wed Sep 25 00:09:57 2019
 @author: randou
 """
 
-class Solution:
+# =============================================================================
+# EASY
+# =============================================================================
+
+# 1. Two Sum
+class Solution1:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for index, num in enumerate(nums):
+            if ((target - num) in nums) and (nums.index(target-num)!=index):
+                return [index, nums.index(target-num)]
+
+# 7. Reverse Integer
+class Solution7:
+    def reverse(self, x: int) -> int:
+        if x<-2**31 or x>(2**31-1):
+            return 0
+        elif x<0:
+            if -int(str(abs(x))[::-1]) in range(-2**31, 2**31-1):
+                return -int(str(abs(x))[::-1])
+            else:
+                return 0
+        elif x>=0:
+            if int(str(abs(x))[::-1]) in range(-2**31, 2**31-1):
+                return int(str(abs(x))[::-1])
+            else:
+                return 0
+
+# 9. Palindrome Number
+class Solution9:
     def isPalindrome(self, x: int) -> bool:
         if x<0:
             return False
@@ -14,9 +42,23 @@ class Solution:
             if sum([str(x)[i]!=str(x)[-(i+1)] for i in range(len(str(x))//2)]) == 0:
                 return True
             else: return False
-       
-        
-class Solution:
+
+# 13. Roman to Integer
+class Solution13:
+    def romanToInt(self, s: str) -> int:
+        symbol_order ={'I':1, 'V':2, 'X':3, 'L':4, 'C':5, 'D':6, 'M':7}
+        symbol_value ={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        sum_list = []
+        for index,letter in enumerate(s):
+            if index != len(s)-1:
+                if symbol_order[letter]>=symbol_order[s[index+1]]:
+                    sum_list += [symbol_value[letter]]
+                else: sum_list += [-symbol_value[letter]]
+            else: sum_list += [symbol_value[letter]]
+        return sum(sum_list)
+
+# 14. Longest Common Prefix
+class Solution14:
     def longestCommonPrefix(self, strs) -> str:
         if not strs:
             return ""
@@ -29,17 +71,40 @@ class Solution:
             else: break
         return ''.join(common_prefix)
 
+# 26. Remove Duplicates from Sorted Array
+class Solution26:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        nums[:len(set(nums))] = list(sorted(set(nums)))
+        return len(set(nums))
 
-class Solution35:
+# 27. Remove Element
+class Solution27:
     def removeElement(self, nums: list, val: int) -> int:
         length = 0
-        for num in nums[::-1]:
+        for num in nums:
             if num==val:
                 length += 1
-                nums.pop(num)
-        return nums
+        for i in range(length):
+            nums.remove(val)
 
+# 28. Implement strStr()
+class Solution28:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        else: 
+            return haystack.find(needle)
 
+# 35. Search Insert Position
+class Solution35:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        position = 0
+        for index,num in enumerate(nums):
+            if num < target:
+                position = index + 1
+        return position         
+
+# 38. Count and Say
 class Solution38:
     def countAndSay(self, n: int) -> str:
         count_say = {1:'1', 2:'11'}
@@ -59,30 +124,18 @@ class Solution38:
                         new.extend([str(len(count)), previous[index]])
             count_say[i] = ''.join(new)
         return count_say[n]
-    
 
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
-        max_list = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)+1):
-                if sum(nums[i:j]) > max_sum:
-                    max_sum = sum(nums[i:j])
-                    max_list = nums[i:j]
-        return max_sum
-
-
+# 58. Length of Last Word
 class Solution58:
     def lengthOfLastWord(self, s: str) -> int:
-        if not s:
+        if not s or not s.split():
             return 0
         else:
             return len(s.split()[-1])
 
-
-class Solution66:
-    def plusOne(self, digits: list) -> list:
+# 66. Plus One
+class Solutio66:
+    def plusOne(self, digits: list[int]) -> list[int]:
         if digits[-1] != 9:
             digits[-1] = digits[-1] + 1
         else:
@@ -98,22 +151,34 @@ class Solution66:
                 digits[-(count_nine+1):] = [digits[-(count_nine+1)]+1] + [0]*count_nine
         return digits
 
+# 67. Add Binary
+class Solution67:
+    def addBinary(self, a: str, b: str) -> str:
+        a_base10 = int(a, 2)
+        b_base10 = int(b, 2)
+        sum_base10 = a_base10 + b_base10
+        sum_base2 = str(bin(sum_base10))
+        return sum_base2[2:]
 
-# 2019.09.25
-# Medium
-# Longest Substring Without Repeating Characters
-class Solution3:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        strtolist = list(s)
-        [y-x for x, y in zip(A[:-1], A[1:])]
+# 69. Sqrt(x)
+class Solution69:
+    def mySqrt(self, x: int) -> int:
+        return int(x**0.5)
+
+
+
+# =============================================================================
+# MEDIUM
+# =============================================================================
 
 
 
 
 
 
-
-
+# =============================================================================
+# HARD
+# =============================================================================
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # LeetCode
-LeetCode Algorithms and Database
+LeetCode Algorithms
+
+### Easy
+1. Two Sum
+7. Reverse Integer
+9. Palindrome Number
+13. Roman to Integer
+14. Longest Common Prefix
+26. Remove Duplicates from Sorted Array
+27. Remove Element
+28. Implement strStr()
+35. Search Insert Position
+38. Count and Say
+58. Length of Last Word
+66. Plus One
+67. Add Binary
+69. Sqrt(x)
+
+
+
+### Medium
+
+
+
+### Hard
+
+
