e0f51f38735d83615bb1a551de2ddf1ccff356d4
Ran <43659207+Ran-Dou@users.noreply.github.com>
2019-10-31 22:45:15 -0400

    Update LeetCode_Algorithms.py

--- LeetCode_Algorithms.py
+++ LeetCode_Algorithms.py
@@ -76,7 +76,7 @@ class Solution20:
     def isValid(self, s: str) -> bool:
         stack = []
         dict = {"]":"[", "}":"{", ")":"("}
-        for char in s:
+        for char in s: 
             if char in dict.values():
                 stack.append(char)
             elif char in dict.keys():
